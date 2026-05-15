d5c60c1a3762004da4dfbef5cc89159af1376812
pashiakntibitla <mounikapashikantibitla@gmail.com>
2026-05-15 17:08:05 +0530

    operators

--- chapter_05_javascrpit_literal/27_string_literals.js
+++ chapter_05_javascrpit_literal/27_string_literals.js
@@ -9,3 +9,21 @@ let uo = "nice to meet you 'mounika'";
 console.log(uo);
 console.log(typeof b, typeof uo);
 console.log(typeof a, typeof ui);
+
+
+// Single quotes
+let single = 'Hello World';
+let withDouble = 'She said "hi"';
+
+// Double quotes
+let double = "Hello World";
+let withSingle = "It's a test";
+
+
+let c = 'c';
+let c1 = 'cc';
+console.log(typeof c);
+console.log(typeof c1);
+console.log(typeof double);
+
+// 'JavaScript prefers to use single code. '
